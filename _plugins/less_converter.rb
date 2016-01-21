9cb15533997c9cdac23955ac4926c19d9a2e651d
Alex Jiang <alex.jiang@outware.com.au>
2016-01-21 16:12:13 +1100

    add reading_time plugin, update less converter plugin, add empty yams front
    add reading_time plugin, update less converter plugin, add empty yams
    front

--- _plugins/less_converter.rb
+++ _plugins/less_converter.rb
@@ -1,25 +1,20 @@
 module Jekyll
-  # Compiled LESS CSS into CSS. You must specify an empty YAML front matter
-  # at the beginning of the file.
-  # .less -> .css
   class LessConverter < Converter
     safe true
-    priority :low
-    pygments_prefix "\n"
-    pygments_suffix "\n"
+    priority :high
 
     def setup
       return if @setup
       require 'less'
       @setup = true
     rescue LoadError
-      STDERR.puts 'You are missing a library required for less. Please run:'
-      STDERR.puts '  $ [sudo] gem install less'
+      STDERR.puts 'You are missing the library required for less. Please run:'
+      STDERR.puts ' $ [sudo] gem install less'
       raise FatalException.new("Missing dependency: less")
     end
 
     def matches(ext)
-      ext =~ /less/i
+      ext =~ /less|lcss/i
     end
 
     def output_ext(ext)
@@ -29,7 +24,8 @@ module Jekyll
     def convert(content)
       setup
       begin
-        Less::Engine.new(content).to_css
+        parser = Less::Parser.new
+        parser = parser.parse(content).to_css
       rescue => e
         puts "Less Exception: #{e.message}"
       end
